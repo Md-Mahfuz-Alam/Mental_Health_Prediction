6ae9dcc06c87344f21c70030bde98322cfb36413
Md-Mahfuz-Alam <83566616+Md-Mahfuz-Alam@users.noreply.github.com>
2023-03-31 05:37:40 +0530

    first commiot

--- app.py
+++ app.py
@@ -6,68 +6,84 @@ from sklearn.ensemble import RandomForestClassifier,StackingClassifier
 from sklearn.naive_bayes import GaussianNB 
 from sklearn.model_selection import train_test_split
 import warnings
-import pickle
+import pickle as pkl
 import streamlit as st
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv("survey.csv")
-# print(data.head(5))
-male_str = ["m", "male-ish","male","maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man", "Cis Male", "cis male"]
-trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]           
-female_str = ["cis female", "f","female" "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail"]
+# data = pd.read_csv("survey.csv")
 
-for (row, col) in data.iterrows():
+# male_str = ["m", "male-ish","male","maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man", "Cis Male", "cis male"]
+# trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]           
+# female_str = ["cis female", "f","female" "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail"]
 
-    if str.lower(col.Gender) in male_str:
-        data['Gender'].replace(to_replace=col.Gender, value='male', inplace=True)
+# for (row, col) in data.iterrows():
 
-    if str.lower(col.Gender) in female_str:
-        data['Gender'].replace(to_replace=col.Gender, value='female', inplace=True)
+#     if str.lower(col.Gender) in male_str:
+#         data['Gender'].replace(to_replace=col.Gender, value='male', inplace=True)
 
-    if str.lower(col.Gender) in trans_str:
-        data['Gender'].replace(to_replace=col.Gender, value='trans', inplace=True)
+#     if str.lower(col.Gender) in female_str:
+#         data['Gender'].replace(to_replace=col.Gender, value='female', inplace=True)
 
-#Get rid of useless
-stk_list = ['A little about you', 'p']
-data = data[~data['Gender'].isin(stk_list)]
-data['Gender']=data['Gender'].map({'male':0,'female':1, 'trans':2})
-data['family_history']=data['family_history'].map({'No':0,'Yes':1})
-data['treatment']=data['treatment'].map({'No':0,'Yes':1})
-data['obs_consequence']=data['obs_consequence'].map({'No':0,'Yes':1})
-data.dropna(axis=0,inplace=True)
-print(data['Gender'].value_counts())
-# print(data.head(5))
-X=data[['Age','Gender','family_history']]
-# print(X.head(5))
-# data = np.array(data)
-Y=data['obs_consequence']
-# X = data[1:,1:-1]
-# y = data[1:, -1]
-# print(y)
-# y = y.astype('int')
-# X = X.astype('int')
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=41)
-# clf1 = KNeighborsClassifier(n_neighbors=1)
-clf2 = RandomForestClassifier(random_state=1)
-clf2.fit(X_train, y_train)
+#     if str.lower(col.Gender) in trans_str:
+#         data['Gender'].replace(to_replace=col.Gender, value='trans', inplace=True)
 
-pickle.dump(clf2,open('model.pkl','wb'))
-model=pickle.load(open('model.pkl','rb'))
+# #Get rid of useless
+# stk_list = ['A little about you', 'p']
+# data = data[~data['Gender'].isin(stk_list)]
+# data['Gender']=data['Gender'].map({'male':0,'female':1, 'trans':2})
+# data['family_history']=data['family_history'].map({'No':0,'Yes':1})
+# data['treatment']=data['treatment'].map({'No':0,'Yes':1})
+# data['obs_consequence']=data['obs_consequence'].map({'No':0,'Yes':1})
+# data.dropna(axis=0,inplace=True)
+# print(data['Gender'].value_counts())
 
-# print(X_test.head(5))
+# X=data[['Age','Gender','family_history']]
+
+# Y=data['obs_consequence']
+
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=41)
+
+# clf2 = RandomForestClassifier(random_state=1)
+# clf2.fit(X_train, y_train)
+
+# pickle.dump(clf2,open('model.pkl','wb'))
+# model=pickle.load(open('model.pkl','rb'))
+
+gender1={'Agender': 0, 'All': 1, 'Androgyne': 2, 'Cis Female': 3, 'Cis Male': 4, 'Cis Man': 5, 'Enby': 6, 'F': 7, 'Femake': 8, 'Female': 9, 'Female ': 10, 'Female (cis)': 11, 'Female (trans)': 12, 'Genderqueer': 13, 'Guy (-ish) ^_^': 14, 'M': 15, 'Mail': 16, 'Make': 17, 'Mal': 18, 'Male': 19, 'Male ': 20, 'Male (CIS)': 21, 'Male-ish': 22, 'Malr': 23, 'Man': 24, 'Nah': 25, 'Neuter': 26, 'Trans woman': 27, 'Trans-female': 28, 'Woman': 29, 'cis male': 30, 'cis-female/femme': 31, 'f': 32, 'femail': 33, 'female': 34, 'fluid': 35, 'm': 36, 'maile': 37, 'male': 38, 'male leaning androgynous': 39, 'msle': 40, 'non-binary': 41, 'ostensibly male, unsure what that really means': 42, 'queer': 43, 'queer/she/they': 44, 'something kinda male?': 45, 'woman': 46}
+
+family_history={'No': 0, 'Yes': 1}
+
+benefits={"Don't know": 0, 'No': 1, 'Yes': 2}
+
+care_options={'No': 0, 'Not sure': 1, 'Yes': 2}
+
+anonymity={"Don't know": 0, 'No': 1, 'Yes': 2}
+
+leave={"Don't know": 0, 'Somewhat difficult': 1, 'Somewhat easy': 2, 'Very difficult': 3, 'Very easy': 4}
+
+work_interfere={"Don't know": 0, 'Never': 1, 'Often': 2, 'Rarely': 3, 'Sometimes': 4}
 
 
 st.title('Mental Health Prediction')
-a=st.text_input("Enter your age",0)
-b=st.text_input("Enter your Gender(Enter 0 for Male and 1 for Female)",0)
-c=st.text_input("Does you have a family history of mental health(Enter 0 for No and 1 for Yes)",0)
+age=int(st.text_input("Enter your age",0))-18
+gender=gender1[st.text_input("Enter your Gender(Enter 0 for Male and 1 for Female)","male")]
+family=family_history[st.radio("Does you have a family history of mental health",("No","yes"))]
+benefit=benefits[st.radio("Does you have benefits",("Don't know","No","Yes"))]
+care=care_options[st.radio("Does you get care options",("No","Not sure","Yes"))]
+anonym=anonymity[st.radio("Any anonymity",("Don't know","No","Yes"))]
+leaves=leave[st.radio("getting leaves",("Don't know","Somewhat difficult","Somewhat easy","Very difficult","Very easy"))]
+work=work_interfere[st.radio("work interference",("Don't know","Never","Often","Rarely","Sometimes"))]
 st.write("Here is Your result ")
 
-result  = model.predict([[int(a),int(b),int(c)]])[0]
 
-if result:
-    st.success('This is a success message!', icon="✅")
+model=pkl.load(open('model.sav','rb'))
+
+result=model.predict([[age,gender,family,benefit,care,anonym,leaves,work]])
+print(result)
+if result[0]:
+     st.error('You are suffered from Mental illness!', icon="🚨")
+   
 else:
-    st.error('This is an error', icon="🚨")
+    st.success('You are OK', icon="✅")
 
 
